e13fcd02e9524eac8e51d39fcf0b3a1c9c54ba1c
Thomas Ricouard <ricouard77@gmail.com>
2011-09-16 23:13:24 +0200

    Initial Commit

---
+++ Raven App Manager/Raven_App_ManagerAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  Raven_App_ManagerAppDelegate.h
+//  Raven App Manager
+//
+//  Created by Thomas Ricouard on 16/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Raven_App_ManagerAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Raven App Manager/Raven_App_ManagerAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  Raven_App_ManagerAppDelegate.m
+//  Raven App Manager
+//
+//  Created by Thomas Ricouard on 16/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Raven_App_ManagerAppDelegate.h"
+
+@implementation Raven_App_ManagerAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
